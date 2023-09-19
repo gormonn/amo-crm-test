51d228772cb361e7a4613a0282bbbf151ff409ee
Dmitriy Aleksandrovich Chernukho <gormonn@redcollar.ru>
2023-09-19 15:45:42 +0300

    fix url path

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -10,8 +10,8 @@ export class AppController {
     return this.appService.getHello();
   }
 
-  @Get('/api/leads')
+  @Get('leads')
   getLeads(): string {
-    return 'leads'
+    return 'leads';
   }
 }
