a84d94b586184a445934b5277cb68be13219f593
Dmitriy Aleksandrovich Chernukho <gormonn@redcollar.ru>
2023-09-19 13:50:12 +0300

    quick-fix nest build

--- back/nestjs/src/app.module.ts
+++ back/nestjs/src/app.module.ts
@@ -1,16 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ApiController } from './api/api.controller';
-import { LeadsController } from './api/leads/leads.controller';
-import { AmoClientService } from './services/amo-client/amo-client.service';
-import { LeadsController } from './controllers/api/leads/leads.controller';
-import { AmoCrmClientService } from './amo-crm-client/amo-crm-client.service';
-import { LeadsController } from './api/leads/leads.controller';
+//import { ApiController } from './controlles/api/api.controller';
+//import { LeadsController } from './api/leads/leads.controller';
+//import { AmoClientService } from './services/amo-client/amo-client.service';
+//import { LeadsController } from './controllers/api/leads/leads.controller';
+//import { AmoCrmClientService } from './amo-crm-client/amo-crm-client.service';
+//import { LeadsController } from './api/leads/leads.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, ApiController, LeadsController],
-  providers: [AppService, AmoCrmClientService, AmoClientService],
+  //  controllers: [AppController, ApiController, LeadsController],
+  //  providers: [AppService, AmoCrmClientService, AmoClientService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
