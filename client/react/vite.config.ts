defd0d9ed65f57fd57ba532f79451d40237d85af
Dmitriy Aleksandrovich Chernukho <gormonn@redcollar.ru>
2023-09-23 17:16:12 +0300

    frontend: add prefix for deploy to GitHub pages

--- client/react/vite.config.ts
+++ client/react/vite.config.ts
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+const base = process.env.PREFIX_PATHS ? '/amo-crm-test/' : undefined;
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths(),react()],
-})
+  base,
+  plugins: [tsconfigPaths(), react()],
+});
