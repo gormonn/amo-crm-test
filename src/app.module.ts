c76586db06d9fe053de84698e6c09f995ae0cc55
Dmitriy Aleksandrovich Chernukho <gormonn@redcollar.ru>
2023-09-23 12:45:54 +0300

    clear code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { HttpModule } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config';
 import { AmoClientService } from './services/amo-client/amo-client.service';
 import { join } from 'path';
 import { getBaseUrl } from './lib';
-//import { ApiController } from './controlles/api/api.controller';
-//import { LeadsController } from './api/leads/leads.controller';
-//import { AmoClientService } from './services/amo-client/amo-client.service';
-//import { LeadsController } from './controllers/api/leads/leads.controller';
-//import { AmoCrmClientService } from './amo-crm-client/amo-crm-client.service';
-//import { LeadsController } from './api/leads/leads.controller';
 
 @Module({
   imports: [
@@ -28,11 +21,8 @@ import { getBaseUrl } from './lib';
         'User-Agent': 'amoCRM-oAuth-client/1.0',
       },
     }),
-    //    HttpModule.register(AXIOS_CONFIG),
   ],
-  //  controllers: [AppController, ApiController, LeadsController],
-  //  providers: [AppService, AmoCrmClientService, AmoClientService],
   controllers: [AppController],
-  providers: [AppService, AmoClientService],
+  providers: [AmoClientService],
 })
 export class AppModule {}
